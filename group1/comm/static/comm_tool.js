62f247546274120700c233e7018f1d279e41f9ac
danbudris <d.c.budris@gmail.com>
2017-10-29 19:37:31 -0400

    updated commtool js with modern method

--- group1/comm/static/comm_tool.js
+++ group1/comm/static/comm_tool.js
@@ -639,3 +639,33 @@ function deleteMessage(msgid) {
 }
 
 
+//Functions to resize Iframes dynamically
+    $(function(){
+    
+        var iFrames = $('iframe');
+      
+    	function iResize() {
+    	
+    		for (var i = 0, j = iFrames.length; i < j; i++) {
+    		  iFrames[i].style.height = iFrames[i].contentWindow.document.body.offsetHeight + 'px';}
+    	    }
+    	    
+        	if ($.browser.safari || $.browser.opera) { 
+        	
+        	   iFrames.load(function(){
+        	       setTimeout(iResize, 0);
+               });
+            
+        	   for (var i = 0, j = iFrames.length; i < j; i++) {
+        			var iSource = iFrames[i].src;
+        			iFrames[i].src = '';
+        			iFrames[i].src = iSource;
+               }
+               
+        	} else {
+        	   iFrames.load(function() { 
+        	       this.style.height = this.contentWindow.document.body.offsetHeight + 'px';
+        	   });
+        	}
+        
+        });
